60ed39a9714dd0a4cba0ad9db4554e60626362b4
Yussuf <yussuf.saumu@gmail.com>
2023-10-15 12:00:22 +0300

    Render every detail available

--- js/index.js
+++ js/index.js
@@ -18,7 +18,31 @@ function renderExercises(exercises){
   });
 }
 
+// Function to render the exercise details
+function renderExercise(exercise){
+  const container = document.querySelector('.exercise-details');
+  
+  container.innerHTML = '';
+  const exerciseType = document.createElement('exerciseType');
+  exerciseType.textContent = (`Exercise type : ${exercise.type}`);
+  container.appendChild(exerciseType);
 
+
+  const exerciseEquip = document.createElement('exerciseEquip');
+  exerciseEquip.textContent = (`Equipment : ${exercise.equipment}`);
+  container.appendChild(exerciseEquip); 
+
+  const exerciseDiff = document.createElement('exerciseDiff');
+  exerciseDiff.textContent = (`Difficulty : ${exercise.difficulty}`);
+  container.appendChild(exerciseDiff); 
+
+
+  const exerciseInstruc = document.createElement('exerciseInstruc');
+  exerciseInstruc.textContent = (`Instructions : ${exercise.instructions}`);
+  container.appendChild(exerciseInstruc); 
+
+
+}
 
 // function to fetch a list of exercises from our API
  function getExercises(){
